bce29c84df756185f720e28fba9de46bc4c6bda8
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-22 22:45:55 +0200

    working with games

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React, { Component } from "react";
 import "./App.css";
+import ScoreSquare from './Components/ScoreSquare/ScoreSquare';
 
 class App extends Component {
-
-  
-render(){
-
-  let nhlDateDay = '2019-04-23';
-
-let xhttpNHL = new XMLHttpRequest();
-xhttpNHL.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    let responseNHL = JSON.parse(this.responseText);
-    console.log(`number of games that day is ${responseNHL.dates[0].games.length}`)
-    for (let i = 0; i < responseNHL.dates[0].games.length; i++) {
-      console.log(responseNHL.dates[0].games[i].teams.away.team.name)
-      console.log(responseNHL.dates[0].games[i].teams.away.score)
-      console.log(responseNHL.dates[0].games[i].teams.home.team.name)
-      console.log(responseNHL.dates[0].games[i].teams.home.score)
+  constructor() {
+    super();
+    this.state = {
+      games: []
     }
   }
-};
-xhttpNHL.open("GET", `https://statsapi.web.nhl.com/api/v1/schedule?date=${nhlDateDay}`, true);
-xhttpNHL.send();
-  return (
-    <div className="App">
-      <h1> Hello, World2 </h1>
-    </div>
-  );
-}
+
+  componentDidMount() {
+    var that = this;
+    let nhlDateDay = '2019-01-02';
+    let prepareGames = {};
+    let xhttpNHL = new XMLHttpRequest();
+    xhttpNHL.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let responseNHL = JSON.parse(this.responseText);
+
+        console.log(`number of games that day is ${responseNHL.dates[0].games.length}`)
+        for (let i = 0; i < responseNHL.dates[0].games.length; i++) {
+          let teamOne = responseNHL.dates[0].games[i].teams.away.team.name;
+          let scoreOne = responseNHL.dates[0].games[i].teams.away.score;
+          let teamTwo = responseNHL.dates[0].games[i].teams.home.team.name;
+          let scoreTwo = responseNHL.dates[0].games[i].teams.away.score;
+          prepareGames[i] = { [teamOne]: scoreOne, [teamTwo]: scoreTwo };
+        }
+        that.setState({ games: prepareGames })
+      }
+    };
+
+    xhttpNHL.open("GET", `https://statsapi.web.nhl.com/api/v1/schedule?date=${nhlDateDay}`, true);
+    xhttpNHL.send();
+  }
+
+  render() {
+    console.log(this.state.games);
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/ScoreSquare/ScoreSquare.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ScoreSquare extends Component {
+
+  
+  render() {
+    return (
+      <div>
+        ScoreSquare
+      </div>
+    );
+  }
+}
+
+export default ScoreSquare;
