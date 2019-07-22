413e2036ecfc4fb9fc244bb7e826cb55243d3410
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-22 22:06:22 +0200

    added simple structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World2 </h1>
-      </div>
-    );
+class App extends Component {
+
+  
+render(){
+
+  let nhlDateDay = '2019-04-23';
+
+let xhttpNHL = new XMLHttpRequest();
+xhttpNHL.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let responseNHL = JSON.parse(this.responseText);
+    console.log(`number of games that day is ${responseNHL.dates[0].games.length}`)
+    for (let i = 0; i < responseNHL.dates[0].games.length; i++) {
+      console.log(responseNHL.dates[0].games[i].teams.away.team.name)
+      console.log(responseNHL.dates[0].games[i].teams.away.score)
+      console.log(responseNHL.dates[0].games[i].teams.home.team.name)
+      console.log(responseNHL.dates[0].games[i].teams.home.score)
+    }
   }
+};
+xhttpNHL.open("GET", `https://statsapi.web.nhl.com/api/v1/schedule?date=${nhlDateDay}`, true);
+xhttpNHL.send();
+  return (
+    <div className="App">
+      <h1> Hello, World2 </h1>
+    </div>
+  );
+}
 }
 
 export default App;
