f17bdf9467c340e90b26b17a35c9b8c34884eea7
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-24 21:04:28 +0200

    added calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
-import GamesContainer from './Containers/GamesContainer/GamesContainer'
+import GamesContainer from './Containers/GamesContainer/GamesContainer';
+import Calendar from 'react-calendar';
 
 class App extends Component {
-  render() {    
-    return (
+  state = {
+    date: new Date(),
+  }
+
+  clicked = () => console.log(this.state.date)
+ 
+  onChange = date => this.setState({ date })
+  render() {
+    return (<div>`
+      <Calendar
+      onChange={this.onChange}
+      value={this.state.date}
+      onClick={this.clicked()}
+      />
       <GamesContainer />
+    </div>
     );
   }
 }
