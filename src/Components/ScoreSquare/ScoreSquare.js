ee2cbfc23fe039d819363e1b6cf0b94bcf85e0fc
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-29 23:55:25 +0200

    startet with logos sprite divs

--- src/Components/ScoreSquare/ScoreSquare.js
+++ src/Components/ScoreSquare/ScoreSquare.js
@@ -7,7 +7,10 @@ class ScoreSquare extends Component {
   render() {   
     return (
       <div className={classes.Square}> 
-        <p>{this.props.teamOneId}</p>{this.props.teamOne} - {this.props.scoreOne}: {this.props.scoreTwo} - {this.props.teamTwo}<p>{this.props.teamTwoId}</p>
+        <div className={classes[`team${this.props.teamOneId}`]}>          
+        </div>
+        {this.props.teamOne} - {this.props.scoreOne}: {this.props.scoreTwo} - {this.props.teamTwo}
+        <div className={classes[`team${this.props.teamTwoId}`]}></div>
       </div>
     );
   }
