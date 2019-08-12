56ca29323ac8dde39ab2ce925a5df909f4bc7fb0
piotrbartnik <piotr.bartnik@hotmail.com>
2019-08-12 22:56:02 +0200

    working with styling

--- src/Components/ScoreSquare/ScoreSquare.js
+++ src/Components/ScoreSquare/ScoreSquare.js
@@ -8,16 +8,16 @@ class ScoreSquare extends Component {
   render() {
     return (
       <div className={this.props.mounted ? [classes.Square, classes.Mounted].join(' ') : classes.Square}>
-        <div className={classes.SquareMargin}>
+        <div className={classes.TeamContainer}>
           <div className={classes[`team${this.props.teamOneId}`]}></div>
-          <div>{this.props.teamOne}</div>
+          <div className={classes.TeamName}>{this.props.teamOne}</div>
         </div>
         <div>
         {this.props.scoreOne}: {this.props.scoreTwo}
         </div>
-        <div className={classes.SquareMargin}>          
+        <div className={classes.TeamContainer}>          
           <div className={classes[`team${this.props.teamTwoId}`]}></div>
-          <div>{this.props.teamTwo}</div>
+          <div className={classes.TeamName}>{this.props.teamTwo}</div>
         </div>
       </div>
     );
