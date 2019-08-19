ebbb7ac1f4b40ef33d7ea95742f2a015ab37a012
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-19 21:49:03 +0000

    Deploy piotrbartnik/nhl-scores to github.com/piotrbartnik/nhl-scores.git:gh-pages

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.+\\.(css|styl|less|sass|scss)$': 'jest-transform-css',
-  },
-};
--- src/store/actions/actionCreators.js
+++
@@ -1,8 +0,0 @@
-import * as actionTypes from './actionTypes';
-
-export const setDate = date => {
-  return {
-    type: actionTypes.SET_DATE,
-    date
-  }
-}
