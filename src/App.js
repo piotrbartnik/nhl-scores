66e1860560af8bd2cb4bc88756aaab63d45a0c62
piotrbartnik <piotr.bartnik@hotmail.com>
2019-09-04 21:52:34 +0200

    added working date tiles

--- src/App.js
+++ src/App.js
@@ -20,9 +20,15 @@ class App extends Component {
 
   }
 
+  
   asyncFunc = () => {
-    const clickedDate = new Date(this.state.date).toLocaleDateString('us-US').replace(/(\d+)\/(\d{2})\/(\d{4})/, "$3-$1-$2");
+    
     console.log(this.state.date)
+  }
+
+  asyncFunc = () => {
+    this.setState({ dateToday: new Date(2018, 2, 5) }) 
+    const clickedDate = new Date(this.state.dateToday).toLocaleDateString('us-US').replace(/(\d+)\/(\d{2})\/(\d{4})/, "$3-$1-$2");
     this.getGames(clickedDate);
   }
 
@@ -69,20 +75,26 @@ class App extends Component {
   render() {
 
     const middleFieldDate = this.state.dateToday;
-
+    console.log(middleFieldDate)
     const daysForCalendar = [];
 
     for (let i = -2; i<3; i++) {
       daysForCalendar.push([new Date(middleFieldDate.getFullYear(), middleFieldDate.getMonth(), middleFieldDate.getDay()+i)])
     };   
     
-    const dateTiles = daysForCalendar.map((x) => {
-      const dateForTile = x.toString().split(' ');
-      return <DateTile dayName={dateForTile[0]} />
+    const dateTiles = daysForCalendar.map(date => {
+      const dateForTile = date.toString().split(' ');
+      return <DateTile 
+        dayName={dateForTile[0]} 
+        dayDate={dateForTile[2][0] == 0 ? dateForTile[2][1] : dateForTile[2]} 
+        dayMonth={dateForTile[1]}
+        dayYear={dateForTile[3]}
+        changeDate={this.asyncFunc}
+        />
     });
 
     return (<div className={classes.mainContainer}>
-      {dateTiles}
+      <div className={classes.DateTilesContainer}>{dateTiles}</div>
       <GamesContainer mounted={this.state.mounted} games={this.state.games} />
     </div>
     );
--- src/Components/SliderCalendar/DateTiles/DateTiles.js
+++ src/Components/SliderCalendar/DateTiles/DateTiles.js
@@ -2,8 +2,11 @@ import React from 'react';
 import classes from './DateTiles.module.scss'
 
 const dateTile = (props) => {
-  const dayDateTable = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-  return (<div className={classes.DateTiles}>{props.dayName}</div>)
+  return (
+  <div className={classes.DateTiles} onClick={props.changeDate}>
+    <div className={classes['DateTiles__Day']} >{props.dayName}</div>
+    <div className={classes['DateTiles__Date']}>{props.dayDate} - {props.dayMonth} - {props.dayYear}</div>
+  </div>)
 };
 
 export default dateTile;
