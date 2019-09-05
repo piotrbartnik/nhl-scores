9831ff211f4ace6196a7a55a9b03e81b11272dfc
piotrbartnik <piotr.bartnik@hotmail.com>
2019-09-06 00:20:23 +0200

    fixing arrow direction

--- src/Components/SliderCalendar/ChangeDaysButton/ChangeDaysButton.js
+++ src/Components/SliderCalendar/ChangeDaysButton/ChangeDaysButton.js
@@ -3,7 +3,7 @@ import classes from './ChangeDaysButton.module.scss'
 
 const changeDaysButton = (props) => {
   return (
-    <button className={classes.ChangeDaysButton} onClick={props.changeDays} arrowDirection={props.arrowDirection}>
+    <button className={classes.ChangeDaysButton} onClick={props.changeDays}>
       {props.arrowDirection ==='left' ? <i className="fas fa-chevron-left"></i> : <i className="fas fa-chevron-right"></i>}</button>
   )
 };
