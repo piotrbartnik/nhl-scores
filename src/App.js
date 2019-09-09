333ca64a040dbe9fff836baf3fd7e02fda980895
piotrbartnik <piotr.bartnik@hotmail.com>
2019-09-09 22:25:15 +0200

    added whole working date tile css selector

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     randomDate: '2019-1-1',
     games: [],
     mounted: false,
-    clickedDate: '',
+    clickedDate: new Date().toLocaleDateString('us-US').replace(/(\d+)\/(\d{2})\/(\d{4})/, "$3-$1-$2"),
   }
 
   asyncFunc = () => {
@@ -71,10 +71,10 @@ class App extends Component {
     for (let i = -2; i < 3; i++) {
       daysForCalendar.push([new Date(middleFieldDate.getFullYear(), middleFieldDate.getMonth(), middleFieldDate.getDate() + i)])
     };
-
+    
     const dateTiles = daysForCalendar.map((date, iteration) => {
       const dateForTile = date.toString().split(' ');
-
+      let activeTileCssToggle = new Date(dateForTile.join(' ')).toLocaleDateString('us-US').replace(/(\d+)\/(\d{2})\/(\d{4})/, "$3-$1-$2") == this.state.clickedDate;
       return <DateTile
         label={dateForTile[2][0] == 0 ? dateForTile[2][1] : dateForTile[2]}
         keyData={iteration}
@@ -83,7 +83,7 @@ class App extends Component {
         dayMonth={dateForTile[1]}
         dayYear={dateForTile[3]}
         changeDate={this.asyncFunc}
-        activeTile={new Date(dateForTile.join(' ')).toLocaleDateString('us-US').replace(/(\d+)\/(\d{2})\/(\d{4})/, "$3-$1-$2") == this.state.clickedDate}
+        activeTile={activeTileCssToggle}
       />
     });
 
