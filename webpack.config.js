1787d6bdc67d576fc540c56b9cf92c4be177dbfe
piotrbartnik <piotr.bartnik@hotmail.com>
2019-09-16 22:51:12 +0200

    configuring eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: "./src/index.js",
+  mode: "development",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['@babel/env'] },
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
       },
       {
         test: /\.(s*)css$/,
-        use: ['style-loader', 'css-loader?modules', 'sass-loader?modules'],
+        use: ["style-loader", "css-loader?modules", "sass-loader?modules"],
       },
       {
         test: /\.(png|jpg|gif)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
             },
@@ -34,19 +34,19 @@ module.exports = {
       },
     ],
   },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
+    contentBase: path.join(__dirname, "public/"),
     port: 3002,
-    publicPath: 'http://localhost:3002',
+    publicPath: "http://localhost:3002",
     hotOnly: true,
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-      favicon: './public/favicon.png',
+      template: "./public/index.html",
+      filename: "./index.html",
+      favicon: "./public/favicon.png",
     }),
   ],
-}
+};
