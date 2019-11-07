a8b98f6a2ee5765ae4b03782c6278c25796c5866
piotrbartnik <piotr.bartnik@hotmail.com>
2019-11-07 07:35:27 +0100

    [feat] added card container

---
+++ src/Components/CardContainer/CardContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './CardContainer.module.scss';
+
+const CardContainer = () => {
+  return (
+    <div className={classes.cardContainer}>
+      <div className={classes.cardContainer__header}>Test</div>
+    </div>
+  );
+};
+
+export default CardContainer;
--- src/Containers/MainPage/MainPage.js
+++ src/Containers/MainPage/MainPage.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import CardContainer from '../../Components/CardContainer/CardContainer';
+import classes from './MainPage.module.scss';
 
 const MainPage = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      Hello
+      <div className={classes.mainContainer}>
+        <CardContainer />
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
