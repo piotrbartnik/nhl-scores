63ba077011570ecb5f2046f24606c2081d7b25ec
piotrbartnik <piotr.bartnik@hotmail.com>
2019-11-10 22:25:17 +0100

    [feat] add todays games to main page

--- src/Containers/MainPage/MainPage.js
+++ src/Containers/MainPage/MainPage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import CardContainer from '../../Components/CardContainer/CardContainer';
 import classes from './MainPage.module.scss';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
 
-const MainPage = () => {
+const MainPage = props => {
+  console.log(props.gamesForTiles);
   return (
     <div>
       Hello
       <div className={classes.mainContainer}>
         <div className={classes.mainContainer__firstRow}>
-          <CardContainer title={'Your favorite team games'} />
+          <CardContainer title={'test00'} />
         </div>
         <div className={classes.mainContainer__secondRow}>
           <CardContainer title={'Stats'} />
@@ -19,4 +22,24 @@ const MainPage = () => {
   );
 };
 
-export default MainPage;
+const mapStateToProps = state => {
+  return {
+    showLoader: state.loader.loading,
+    shouldGameTilesMount: state.mountGameTiles.mounted,
+    gamesForTiles: state.gamesFromApiSchedule.gamesApiSchedule,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getGamesForGameTiles: payload =>
+      dispatch(actions.fetchGamesForTiles(payload)),
+    setGamesNumberForSliderCalendar: payload =>
+      dispatch(actions.fetchNumberOfGamesForSlider(payload)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainPage);
