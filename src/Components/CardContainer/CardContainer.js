41a84a10ac46d0e4b280327bf3fcae74b289e37b
piotrbartnik <piotr.bartnik@hotmail.com>
2019-11-12 22:06:21 +0100

    [feat] added first working version of scores on main page

--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -5,6 +5,7 @@ const CardContainer = props => {
   return (
     <div className={classes.cardContainer}>
       <div className={classes.cardContainer__header}>{props.title}</div>
+      <div className={classes.cardContainer__content}>{props.content}</div>
     </div>
   );
 };
--- src/Containers/MainPage/MainPage.js
+++ src/Containers/MainPage/MainPage.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CardContainer from '../../Components/CardContainer/CardContainer';
 import classes from './MainPage.module.scss';
+import moment from 'moment';
+import ScoreSquare from '../../Components/ScoreSquare/ScoreSquare';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions';
 
 const MainPage = props => {
-  console.log(props.gamesForTiles);
+  useEffect(() => {
+    props.getGamesForGameTiles(moment(new Date()).format('YYYY-MM-DD'));
+  }, []);
+
+  const testGames = (
+    <div className={classes.GamesContainer}>
+      {props.gamesForTiles.length > 0
+        ? props.gamesForTiles.map((games, i) => (
+            <ScoreSquare
+              mounted={true}
+              key={i}
+              teamOneId={games[0][2]}
+              teamOne={games[0][0]}
+              scoreOne={games[0][1]}
+              teamTwo={games[1][0]}
+              scoreTwo={games[1][1]}
+              teamTwoId={games[1][2]}
+              venue={games[2]}
+            />
+          ))
+        : null}
+    </div>
+  );
+
   return (
     <div>
       Hello
       <div className={classes.mainContainer}>
         <div className={classes.mainContainer__firstRow}>
-          <CardContainer title={'test00'} />
+          <CardContainer title={'test00'} content={testGames} />
         </div>
         <div className={classes.mainContainer__secondRow}>
           <CardContainer title={'Stats'} />
