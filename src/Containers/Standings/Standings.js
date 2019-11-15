50c794e28fc33bebfebaafd85be02298f357b9f2
piotrbartnik <piotr.bartnik@hotmail.com>
2019-11-15 08:18:01 +0100

    [feat] add react table

--- src/Containers/Standings/Standings.js
+++ src/Containers/Standings/Standings.js
@@ -1,10 +1,44 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+// import React from 'react';
+// // import PropTypes from 'prop-types';
+// import ReactTable from 'react-table'
+// import 'react-table/react-table.css'
 
-const Standings = () => {
-  return <div>Here will standings go</div>;
-};
+// const Standings = () => {
+//   render() {
+//     const data = [{
+//       name: 'Tanner Linsley',
+//       age: 26,
+//       friend: {
+//         name: 'Jason Maurer',
+//         age: 23,
+//       }
+//     },{
+//       ...
+//     }]
 
-// Standings.propTypes = {};
+//     const columns = [{
+//       Header: 'Name',
+//       accessor: 'name' // String-based value accessors!
+//     }, {
+//       Header: 'Age',
+//       accessor: 'age',
+//       Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+//     }, {
+//       id: 'friendName', // Required because our accessor is not a string
+//       Header: 'Friend Name',
+//       accessor: d => d.friend.name // Custom value accessors!
+//     }, {
+//       Header: props => <span>Friend Age</span>, // Custom header components!
+//       accessor: 'friend.age'
+//     }]
 
-export default Standings;
+//   }
+//   return <div><ReactTable
+//   data={data}
+//   columns={columns}
+// /></div>;
+// };
+
+// // Standings.propTypes = {};
+
+// export default Standings;
