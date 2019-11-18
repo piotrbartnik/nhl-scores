39ccb2f02d25b03fcd13f190bfd3e485f15637b6
piotrbartnik <piotr.bartnik@hotmail.com>
2019-11-18 22:17:07 +0100

    [chore]: added working react-table

--- src/Containers/Standings/Standings.js
+++ src/Containers/Standings/Standings.js
@@ -1,44 +1,41 @@
-// import React from 'react';
-// // import PropTypes from 'prop-types';
-// import ReactTable from 'react-table'
-// import 'react-table/react-table.css'
+import React from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import { Component } from 'react';
+class Standings extends Component {
+  render() {
+    const data = [
+      {
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        },
+      },
+    ];
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name', // String-based value accessors!
+      },
+      {
+        Header: 'Age',
+        accessor: 'age',
+        Cell: props => <span className="number">{props.value}</span>, // Custom cell components!
+      },
+      {
+        id: 'friendName', // Required because our accessor is not a string
+        Header: 'Friend Name',
+        accessor: d => d.friend.name, // Custom value accessors!
+      },
+      {
+        Header: () => <span>Friend Age</span>, // Custom header components!
+        accessor: 'friend.age',
+      },
+    ];
 
-// const Standings = () => {
-//   render() {
-//     const data = [{
-//       name: 'Tanner Linsley',
-//       age: 26,
-//       friend: {
-//         name: 'Jason Maurer',
-//         age: 23,
-//       }
-//     },{
-//       ...
-//     }]
-
-//     const columns = [{
-//       Header: 'Name',
-//       accessor: 'name' // String-based value accessors!
-//     }, {
-//       Header: 'Age',
-//       accessor: 'age',
-//       Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-//     }, {
-//       id: 'friendName', // Required because our accessor is not a string
-//       Header: 'Friend Name',
-//       accessor: d => d.friend.name // Custom value accessors!
-//     }, {
-//       Header: props => <span>Friend Age</span>, // Custom header components!
-//       accessor: 'friend.age'
-//     }]
-
-//   }
-//   return <div><ReactTable
-//   data={data}
-//   columns={columns}
-// /></div>;
-// };
-
-// // Standings.propTypes = {};
-
-// export default Standings;
+    return <ReactTable data={data} columns={columns} />;
+  }
+}
+export default Standings;
