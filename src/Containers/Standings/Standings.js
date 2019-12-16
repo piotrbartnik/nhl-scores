02330b139fe563c120bbd3d02289e0492f0b67cf
piotrbartnik <piotr.bartnik@hotmail.com>
2019-12-16 21:35:09 +0100

    [chore]: get rid of react table

--- src/Containers/Standings/Standings.js
+++ src/Containers/Standings/Standings.js
@@ -1,53 +1,15 @@
 import React from 'react';
-import ReactTable from 'react-table';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions';
 import 'react-table/react-table.css';
-import classes from './Standings.module.scss';
 class Standings extends Component {
   componentDidMount() {
     this.props.getDataForStandingsTable();
   }
 
   render() {
-    const data = [];
-
-    return (
-      <ReactTable
-        className={classes.table}
-        data={data}
-        columns={[
-          {
-            Header: 'Rank',
-          },
-          {
-            Header: 'Team',
-          },
-          {
-            Header: 'GP',
-          },
-          {
-            Header: 'W',
-          },
-          {
-            Header: 'L',
-          },
-          {
-            Header: 'OT',
-          },
-          {
-            Header: 'GS',
-          },
-          {
-            Header: 'GL',
-          },
-          {
-            Header: 'PTS',
-          },
-        ]}
-      />
-    );
+    return <div>Table will be here</div>;
   }
 }
 
