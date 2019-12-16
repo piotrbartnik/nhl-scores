dd355836cd9294e3ea864b80ee71dce7749b97fc
piotrbartnik <piotr.bartnik@hotmail.com>
2019-12-16 21:37:54 +0100

    [chore]: deleted react table css

--- src/Containers/Standings/Standings.js
+++ src/Containers/Standings/Standings.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions';
-import 'react-table/react-table.css';
 class Standings extends Component {
   componentDidMount() {
     this.props.getDataForStandingsTable();
