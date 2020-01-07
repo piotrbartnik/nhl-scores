40d9518b9730e41b4a5258aff8ceb91c57916387
piotrbartnik <piotr.bartnik@hotmail.com>
2020-01-07 22:04:31 +0100

    [chore]: add simple table structure

--- src/Containers/Standings/Standings.js
+++ src/Containers/Standings/Standings.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import classes from './Standings.module.scss';
 import * as actions from '../../store/actions';
 class Standings extends Component {
   componentDidMount() {
@@ -9,7 +10,19 @@ class Standings extends Component {
 
   render() {
     console.log(this.props.standingsData);
-    return <div>Table will be here</div>;
+    return (
+      <div>
+        Table will be here
+        <div className={classes.tableContainer}>
+          <div className={classes.tableHeader}>
+            {Object.keys(this.props.standingsData)[0]}
+          </div>
+          {/* <div className={classes.tableStats}>
+            <div>{this.props.standingsData.Metropolitan}</div>
+          </div> */}
+        </div>
+      </div>
+    );
   }
 }
 
